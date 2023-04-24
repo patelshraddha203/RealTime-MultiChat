5cb681ec6a2a0c1d7831745fd5912521e7a8a7f8
Shraddha Singh Patel <patelshraddha203@gmail.com>
2023-04-24 16:48:16 +0530

    Add files via upload

---
+++ RealtimeMultichat/common/header.h
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+void set_name(int , char);
+void shared_print(string, bool);
+int broadcast_message(string , int);
+int broadcast_message(int , int );
+void end_connection(int );
+void handle_client(int, int );
+void catch_ctrl_c(int);
+string color(int code);
+int eraseText(int );
+void send_message(int);
+void recv_message(int);
